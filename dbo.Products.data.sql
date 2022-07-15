9f117d0b01e277ba94f3d169acee8655de08cff7
someshgollangi <41678979+someshgollangi@users.noreply.github.com>
2022-07-15 12:11:53 -0400

    Add files via upload

---
+++ dbo.Products.data.sql
@@ -0,0 +1,10 @@
+SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (1, N'Clothing', N'~/Images/Categories/Clothing.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (2, N'Electronics', N'~/Images/Categories/Electronics.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (3, N'Mobiles', N'~/Images/Categories/Mobiles.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (4, N'Watches', N'~/Images/Categories/Watches.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (5, N'Laptops', N'~/Images/Categories/Laptops.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (6, N'Shoes', N'~/Images/Categories/Shoes.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (7, N'Furniture', N'~/Images/Categories/Furniture.jfif')
+INSERT INTO [dbo].[Products] ([Productid], [Productname], [Productimage]) VALUES (8, N'KidsFashion', N'~/Images/Categories/KidsFashion.jfif')
+SET IDENTITY_INSERT [dbo].[Products] OFF
---
+++ dbo.Role_tbl.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[Role_tbl] ON
+INSERT INTO [dbo].[Role_tbl] ([RoleId], [RoleName]) VALUES (1, N'Admin')
+INSERT INTO [dbo].[Role_tbl] ([RoleId], [RoleName]) VALUES (2, N'User')
+SET IDENTITY_INSERT [dbo].[Role_tbl] OFF
